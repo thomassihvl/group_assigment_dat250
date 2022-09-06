0c38f1dab936a764c4e26feb050f7c1b70a03b7c
König <harald.koenig@fhdw.de>
2022-09-06 09:34:14 +0200

    Short test

--- eclipselink/experiment-1/src/main/java/no/hvl/dat250/jpa/assignment2/Main.java
+++ eclipselink/experiment-1/src/main/java/no/hvl/dat250/jpa/assignment2/Main.java
@@ -3,6 +3,6 @@ package no.hvl.dat250.jpa.assignment2;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: implement this....
+        // TODO: implement this...
     }
 }
