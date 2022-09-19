d4000f8a4fa67338caf9f168b65d7c822b0e4cb6
Jonas Hammerseth <jonashammerseth@hotmail.com>
2022-09-19 12:52:59 +0200

    added test

--- experiment-2/src/test/java/no/hvl/dat250/assignment2/driver/MainTest.java
+++ experiment-2/src/test/java/no/hvl/dat250/assignment2/driver/MainTest.java
@@ -34,59 +34,29 @@ public class MainTest {
 
         EntityManager em = factory.createEntityManager();
 
-        // Load person
-        Person person = em.find(Person.class, 1L);
+        // Load consumer
+        Consumer consumer = em.find(Consumer.class, 1L);
 
-        // Test person data
-        assertThat(person.getName(), is("Max Mustermann"));
+        // Test consumer data
+        assertThat(consumer.getUsername(), is("BingBongFML"));
 
-        // Test address
-        assertThat(person.getAddresses().size(), is(1));
-        Address address = person.getAddresses().iterator().next();
+        // Test polls
+        assertThat(consumer.getOwnedPrivatePolls().size(), is(1));
+        assertThat(consumer.getOwnedPublicPolls().size(), is(1));
 
-        assertThat(address.getStreet(), is("Inndalsveien"));
-        assertThat(address.getNumber(), is(28));
-        //assertThat(address.getOwners(), is(Set.of(person)));
-        assertThat(address.getOwners(), is(Set.of(person)));
 
-        
-        // Test credit cards
-        assertThat(person.getCreditCards().size(), is(2));
-        CreditCard firstCard = getCardWithNumber(person, 12345);
-        CreditCard secondCard = getCardWithNumber(person, 123);
+        // Test public polls
+        PublicPoll publicPoll = em.find(PublicPoll.class, 3L);
+        PrivatePoll privatePoll = em.find(PrivatePoll.class, 2L);
 
-        assertThat(firstCard.getNumber(), is(12345));
-        assertThat(firstCard.getBalance(), is(-5000));
-        assertThat(firstCard.getLimit(), is(-10000));
+        assertThat(publicPoll.getTotalCount(), is(4));
+        assertThat(publicPoll.getStatus(), is(Status.ACTIVE));
+        assertThat(publicPoll.getConsumer(), is(consumer));
 
-        assertThat(secondCard.getNumber(), is(123));
-        assertThat(secondCard.getBalance(), is(1));
-        assertThat(secondCard.getLimit(), is(2000));
+        assertThat(privatePoll.getPollQuestion(), is("Is this really a Private Poll?"));
+        assertThat(privatePoll.getTotalCount(), is(4));
+        assertThat(privatePoll.getStatus(), is(Status.ACTIVE));
+        assertThat(privatePoll.getConsumer(), is(consumer));
 
-        // Test pincode
-        Pincode firstCardPincode = firstCard.getPincode();
-
-        assertThat(firstCardPincode.getId(), is(secondCard.getPincode().getId())); // Pincode objects of the two cards are identical!
-        assertThat(firstCardPincode.getPincode(), is("123"));
-        assertThat(firstCardPincode.getCount(), is(1));
-
-        // Test bank
-        Bank bank = firstCard.getOwningBank();
-        assertThat(bank.getId(), is(secondCard.getOwningBank().getId())); // Bank objects of the two cards are identical!
-        assertThat(bank.getName(), is("Pengebank"));
-        //assertThat(bank.getOwnedCards(), is(Set.of(firstCard, secondCard)));
-    }
-
-    private CreditCard getCardWithNumber(Person person, int cardNumber) {
-        Optional<CreditCard> optionalCard = person.getCreditCards().stream()
-                .filter(creditCard -> creditCard.getNumber() == cardNumber)
-                .findFirst();
-        if (optionalCard.isEmpty()) {
-            throw new RuntimeException(
-                    String.format("Card with number %s was not found for the person %s!",
-                            cardNumber,
-                            person.getName()));
-        }
-        return optionalCard.get();
     }
 }
